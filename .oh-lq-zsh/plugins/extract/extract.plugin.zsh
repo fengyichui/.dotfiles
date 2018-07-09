83eab921ce60cd07e9b4914560932cd4d5a487d6
fengyichui <fengyichui@gmail.com>
2018-07-09 20:38:39 +0800

    zsh: extract add -c option to compress files

--- .oh-lq-zsh/plugins/extract/extract.plugin.zsh
+++ .oh-lq-zsh/plugins/extract/extract.plugin.zsh
@@ -1,91 +1,109 @@
 alias x=extract
 
 extract() {
-    local remove_archive
-    local list_archive
-    local success
-    local extract_dir
 
     if (( $# == 0 )); then
 		cat <<-'EOF' >&2
 			Usage: extract [-option] [file ...]
-
+			Default: Extract archive file
 			Options:
 			    -r, --remove    Remove archive after unpacking.
 			    -l, --list      List archive files' name.
+			    -c, --compress  Compress files/directories to archive file
+			                    extract -c <archive-name> <files>
 		EOF
     fi
 
-    remove_archive=1
-    if [[ "$1" == "-r" ]] || [[ "$1" == "--remove" ]]; then
-        remove_archive=0
+    if [[ "$1" == "-c" ]] || [[ "$1" == "--compress" ]]; then
         shift
-    fi
 
-    list_archive=0
-    if [[ "$1" == "-l" ]] || [[ "$1" == "--list" ]]; then
-        list_archive=1
-        shift
-    fi
+        # compress
+        case "$1" in
+            (*.zip) zip -r "$@" ;;
+            (*.7z) 7za a "$@" ;;
+            (*.rar) ;;
+            (*) tar cavf "$@" ;;
+        esac
 
-    while (( $# > 0 )); do
-        if [[ ! -f "$1" ]]; then
-            echo "extract: '$1' is not a valid file" >&2
+    else
+
+        local remove_archive
+        local list_archive
+        local success
+        local extract_dir
+
+        remove_archive=1
+        if [[ "$1" == "-r" ]] || [[ "$1" == "--remove" ]]; then
+            remove_archive=0
             shift
-            continue
         fi
 
-        if [[ "$list_archive" == "1" ]]; then
-            case "$1" in
-                (*.zip|*.war|*.jar|*.ipsw|*.xpi|*.apk) unzip -l "$1" | less ;;
-                (*.gz) gunzip -l "$1" | less ;;
-                (*.rar) unrar l "$1" | less ;;
-                (*.7z) 7za l "$1" | less ;;
-                (*) tar tavf "$1" | less ;;
-            esac
-        else
-            success=0
-            extract_dir="${1:t:r}"
-            case "$1" in
-                (*.tar.gz|*.tgz) (( $+commands[pigz] )) && { pigz -dc "$1" | tar xv } || tar zxvf "$1" ;;
-                (*.tar.bz2|*.tbz|*.tbz2) tar xvjf "$1" ;;
-                (*.tar.xz|*.txz)
-                    tar --xz --help &> /dev/null \
-                    && tar --xz -xvf "$1" \
-                    || xzcat "$1" | tar xvf - ;;
-                (*.tar.zma|*.tlz)
-                    tar --lzma --help &> /dev/null \
-                    && tar --lzma -xvf "$1" \
-                    || lzcat "$1" | tar xvf - ;;
-                (*.tar) tar xvf "$1" ;;
-                (*.gz) (( $+commands[pigz] )) && pigz -d "$1" || gunzip "$1" ;;
-                (*.bz2) bunzip2 "$1" ;;
-                (*.xz) unxz "$1" ;;
-                (*.lzma) unlzma "$1" ;;
-                (*.Z) uncompress "$1" ;;
-                (*.zip|*.war|*.jar|*.sublime-package|*.ipsw|*.xpi|*.apk) unzip "$1" -d $extract_dir ;;
-                (*.rar) unrar x -ad "$1" ;;
-                (*.7z) 7za x "$1" ;;
-                (*.deb)
-                    mkdir -p "$extract_dir/control"
-                    mkdir -p "$extract_dir/data"
-                    cd "$extract_dir"; ar vx "../${1}" > /dev/null
-                    cd control; tar xzvf ../control.tar.gz
-                    cd ../data; extract ../data.tar.*
-                    cd ..; rm *.tar.* debian-binary
-                    cd ..
-                ;;
-                (*)
-                    echo "extract: '$1' cannot be extracted" >&2
-                    success=1
-                ;;
-            esac
-
-            (( success = $success > 0 ? $success : $? ))
-            (( $success == 0 )) && (( $remove_archive == 0 )) && rm "$1"
+        list_archive=0
+        if [[ "$1" == "-l" ]] || [[ "$1" == "--list" ]]; then
+            list_archive=1
+            shift
         fi
 
-        shift
-    done
+        while (( $# > 0 )); do
+            if [[ ! -f "$1" ]]; then
+                echo "extract: '$1' is not a valid file" >&2
+                shift
+                continue
+            fi
+
+            if [[ "$list_archive" == "1" ]]; then
+                # list
+                case "$1" in
+                    (*.zip|*.war|*.jar|*.ipsw|*.xpi|*.apk) unzip -l "$1" | less ;;
+                    (*.rar) unrar l "$1" | less ;;
+                    (*.7z) 7za l "$1" | less ;;
+                    (*) tar tavf "$1" | less ;;
+                esac
+            else
+                # extract
+                success=0
+                extract_dir="${1:t:r}"
+                case "$1" in
+                    (*.tar.gz|*.tgz) (( $+commands[pigz] )) && { pigz -dc "$1" | tar xv } || tar zxvf "$1" ;;
+                    (*.tar.bz2|*.tbz|*.tbz2) tar xvjf "$1" ;;
+                    (*.tar.xz|*.txz)
+                        tar --xz --help &> /dev/null \
+                        && tar --xz -xvf "$1" \
+                        || xzcat "$1" | tar xvf - ;;
+                    (*.tar.zma|*.tlz)
+                        tar --lzma --help &> /dev/null \
+                        && tar --lzma -xvf "$1" \
+                        || lzcat "$1" | tar xvf - ;;
+                    (*.tar) tar xvf "$1" ;;
+                    (*.gz) (( $+commands[pigz] )) && pigz -d "$1" || gunzip "$1" ;;
+                    (*.bz2) bunzip2 "$1" ;;
+                    (*.xz) unxz "$1" ;;
+                    (*.lzma) unlzma "$1" ;;
+                    (*.Z) uncompress "$1" ;;
+                    (*.zip|*.war|*.jar|*.sublime-package|*.ipsw|*.xpi|*.apk) unzip "$1" -d $extract_dir ;;
+                    (*.rar) unrar x -ad "$1" ;;
+                    (*.7z) 7za x "$1" ;;
+                    (*.deb)
+                        mkdir -p "$extract_dir/control"
+                        mkdir -p "$extract_dir/data"
+                        cd "$extract_dir"; ar vx "../${1}" > /dev/null
+                        cd control; tar xzvf ../control.tar.gz
+                        cd ../data; extract ../data.tar.*
+                        cd ..; rm *.tar.* debian-binary
+                        cd ..
+                    ;;
+                    (*)
+                        echo "extract: '$1' cannot be extracted" >&2
+                        success=1
+                    ;;
+                esac
+
+                (( success = $success > 0 ? $success : $? ))
+                (( $success == 0 )) && (( $remove_archive == 0 )) && rm "$1"
+            fi
+
+            shift
+        done
+    fi
 }
 
