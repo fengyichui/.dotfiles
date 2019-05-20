bb393d42951aba1dcdf85503bd4c19d9a9083912
fengyichui <fengyichui@gmail.com>
2019-05-20 10:05:42 +0800

    gdb: fix ignore-errors issue

--- .gdb/gdbinit.py
+++ .gdb/gdbinit.py
@@ -186,13 +186,14 @@ Only one-line commands are supported.
 This is primarily useful in scripts."""
 
     def __init__ (self):
-        super (IgnoreErrorsCommand, self).__init__ ("ignore-errors", gdb.COMMAND_OBSCURE, gdb.COMPLETE_COMMAND)
+        super (self.__class__, self).__init__ ("ignore-errors", gdb.COMMAND_OBSCURE, gdb.COMPLETE_COMMAND)
 
     def invoke (self, arg, from_tty):
         try:
             gdb.execute (arg, from_tty)
-        except:
-            pass
+        except Exception as e:
+            print(e)
+
 
 ######################################################################
 # CLASS
