890136b65a3a809d48ead2584cbdb721fa1ba354
fengyichui <fengyichui@gmail.com>
2019-11-08 17:47:29 +0800

    zsh: update omz_termsupport_preexec

--- .oh-my-zsh/lib/termsupport.zsh
+++ .oh-my-zsh/lib/termsupport.zsh
@@ -72,7 +72,13 @@ function omz_termsupport_preexec {
   local CMD=${1[(wr)^(*=*|sudo|ssh|mosh|rake|-*)]:gs/%/%%}
   local LINE="${2:gs/%/%%}"
 
-  title "[$CMD] $ZSH_THEME_TERM_TAB_TITLE_IDLE $ZSH_THEME_TERM_TITLE_IDLE [%100>...>$LINE%<<]" #qli<>
+  if [[ -n "$GIT_CURRENT_INFO" ]]; then
+    local INFO=$GIT_CURRENT_INFO
+  else
+    local INFO=$ZSH_THEME_TERM_TAB_TITLE_IDLE
+  fi
+
+  title "[$CMD] $INFO $ZSH_THEME_TERM_TITLE_IDLE [%100>...>$LINE%<<]" #qli<>
 }
 
 precmd_functions+=(omz_termsupport_precmd)
