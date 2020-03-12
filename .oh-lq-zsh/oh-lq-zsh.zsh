ec4486d9765d3db90d488c0400453fc2ae50c14e
fengyichui <fengyichui@gmail.com>
2020-03-12 13:51:04 +0800

    zsh: history-substring-search more bindkey

--- .oh-lq-zsh/oh-lq-zsh.zsh
+++ .oh-lq-zsh/oh-lq-zsh.zsh
@@ -31,4 +31,6 @@ ZSH_HIGHLIGHT_DIRS_BLACKLIST+=(//)
 # history-substring-search
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
 
