2ff4d3bd58b1ae0088832bea5069c32ef50ac3d4
fengyichui <fengyichui@gmail.com>
2021-01-03 15:45:53 +0800

    install-default-termux.sh: better

--- .bin/install-default-termux.sh
+++ .bin/install-default-termux.sh
@@ -14,14 +14,16 @@
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
 sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
 sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
-apt update && apt upgrade
+apt --yes update
+apt --yes upgrade
 
 # Storage
-apt install termux-tools
+apt --yes install termux-tools
+rm -rf ~/storage
 termux-setup-storage
 
 # clipboard and other feature support
-apt install termux-api
+apt --yes install termux-api
 
 # Package
 apt --yes install \
@@ -37,8 +39,13 @@ apt --yes install \
     openssh \
     ncurses-utils \
 
-# install my dotfiles
-git clone --depth 1 https://github.com/fengyichui/.dotfiles.git
+# install my dotfiles and other package files
+git clone --depth 1 https://gitee.com/fengyichui/dotfiles.git .dotfiles
+
+# gitee to github
+sed -i -e 's#https://gitee.com/fengyichui/dotfiles.git#https://github.com/fengyichui/.dotfiles.git#' .dotfiles/.git/config
+
+# install dotfiles
 ./.dotfiles/.bin/install-dotfiles
 
 # default shell
