76e9ea5eef4a079d46f922da29f46ddf4ad4f319
fengyichui <fengyichui@gmail.com>
2021-06-17 20:24:31 +0800

    wsl-cmd: better 4-edit-with-vim-menu-add-wsl.js

--- .bin/cygwin/cmd/4-edit-with-vim-menu-add-wsl.js
+++ .bin/cygwin/cmd/4-edit-with-vim-menu-add-wsl.js
@@ -4,8 +4,8 @@ LOCALAPPDATA = WshShell.ExpandEnvironmentStrings("%LOCALAPPDATA%");
 APPDATA      = WshShell.ExpandEnvironmentStrings("%APPDATA%")
 
 WslIcon      = "\"" + LOCALAPPDATA + "\\wsltty\\wsl.ico\""
-WslVimCmd    = "\"" + LOCALAPPDATA + "\\wsltty\\bin\\mintty.exe\" --WSL --configdir=\"" + APPDATA + "\\wsltty\" --title \"[vim] %1\"    --exec bash -c \"vim    \\\"$(wslpath \\\"$(echo -E '%1' | sed 's/\\\\\\\\/\\\\\\\\\\\\\\\\/g')\\\")\\\"\""
-WslVimBinCmd = "\"" + LOCALAPPDATA + "\\wsltty\\bin\\mintty.exe\" --WSL --configdir=\"" + APPDATA + "\\wsltty\" --title \"[vim -b] %1\" --exec bash -c \"vim -b \\\"$(wslpath \\\"$(echo -E '%1' | sed 's/\\\\\\\\/\\\\\\\\\\\\\\\\/g')\\\")\\\"\""
+WslVimCmd    = "\"" + LOCALAPPDATA + "\\wsltty\\bin\\mintty.exe\" --WSL --icon " + WslIcon + " --configdir=\"" + APPDATA + "\\wsltty\" --title \"[*VIM*] %1\"    --exec bash -c \"vim    \\\"$(wslpath \\\"$(echo -E '%1' | sed 's/\\\\\\\\/\\\\\\\\\\\\\\\\/g')\\\")\\\"\""
+WslVimBinCmd = "\"" + LOCALAPPDATA + "\\wsltty\\bin\\mintty.exe\" --WSL --icon " + WslIcon + " --configdir=\"" + APPDATA + "\\wsltty\" --title \"[*VIM*] -b %1\" --exec bash -c \"vim -b \\\"$(wslpath \\\"$(echo -E '%1' | sed 's/\\\\\\\\/\\\\\\\\\\\\\\\\/g')\\\")\\\"\""
 
 WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl\\", "Edit with &Vim in WSL", "REG_SZ");
 WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl\\icon", WslIcon, "REG_SZ");
