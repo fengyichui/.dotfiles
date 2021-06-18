4db0d78378b39d77713a9e00ac490eee64bf6ee0
fengyichui <fengyichui@gmail.com>
2021-06-18 10:25:52 +0800

    wsl-cmd: better 4-edit-with-vim-menu-add-wsl.js

--- .bin/cygwin/cmd/4-edit-with-vim-menu-add-wsl.js
+++ .bin/cygwin/cmd/4-edit-with-vim-menu-add-wsl.js
@@ -3,15 +3,15 @@ var WshShell = new ActiveXObject("WScript.Shell");
 LOCALAPPDATA = WshShell.ExpandEnvironmentStrings("%LOCALAPPDATA%");
 APPDATA      = WshShell.ExpandEnvironmentStrings("%APPDATA%")
 
-WslIcon      = "\"" + LOCALAPPDATA + "\\wsltty\\wsl.ico\""
-WslVimCmd    = "\"" + LOCALAPPDATA + "\\wsltty\\bin\\mintty.exe\" --WSL --icon " + WslIcon + " --configdir=\"" + APPDATA + "\\wsltty\" --title \"[*VIM*] %1\"    --exec bash -c \"vim    \\\"$(wslpath \\\"$(echo -E '%1' | sed 's/\\\\\\\\/\\\\\\\\\\\\\\\\/g')\\\")\\\"\""
-WslVimBinCmd = "\"" + LOCALAPPDATA + "\\wsltty\\bin\\mintty.exe\" --WSL --icon " + WslIcon + " --configdir=\"" + APPDATA + "\\wsltty\" --title \"[*VIM*] -b %1\" --exec bash -c \"vim -b \\\"$(wslpath \\\"$(echo -E '%1' | sed 's/\\\\\\\\/\\\\\\\\\\\\\\\\/g')\\\")\\\"\""
+WslVimIcon   = "\"" + LOCALAPPDATA + "\\wsltty\\vim.ico\""
+WslVimCmd    = "\"" + LOCALAPPDATA + "\\wsltty\\bin\\mintty.exe\" --WSL --icon " + WslVimIcon + " --configdir=\"" + APPDATA + "\\wsltty\" --title \"[*VIM*] %1\"    --exec bash -c \"vim    \\\"$(wslpath \\\"$(echo -E '%1' | sed 's/\\\\\\\\/\\\\\\\\\\\\\\\\/g')\\\")\\\"\""
+WslVimBinCmd = "\"" + LOCALAPPDATA + "\\wsltty\\bin\\mintty.exe\" --WSL --icon " + WslVimIcon + " --configdir=\"" + APPDATA + "\\wsltty\" --title \"[*VIM*] -b %1\" --exec bash -c \"vim -b \\\"$(wslpath \\\"$(echo -E '%1' | sed 's/\\\\\\\\/\\\\\\\\\\\\\\\\/g')\\\")\\\"\""
 
 WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl\\", "Edit with &Vim in WSL", "REG_SZ");
-WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl\\icon", WslIcon, "REG_SZ");
+WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl\\icon", WslVimIcon, "REG_SZ");
 WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl\\command\\", WslVimCmd, "REG_SZ");
 
 WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl-binary\\", "Edit with &Vim (Binary) in WSL", "REG_SZ");
-WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl-binary\\icon", WslIcon, "REG_SZ");
+WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl-binary\\icon", WslVimIcon, "REG_SZ");
 WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl-binary\\command\\", WslVimBinCmd, "REG_SZ");
 
